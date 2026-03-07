f6f30ded4785f74c9f7b47669d83951afc8442a1
maobenmo <hit.tingtingsong@gmail.com>
2026-03-07 23:05:21 +1100

    small fix

--- src/app/components/SignUp/components/Form/components/Dialog/Dialog.jsx
+++ src/app/components/SignUp/components/Form/components/Dialog/Dialog.jsx
@@ -11,7 +11,7 @@ const Dialog = ({ children }) => {
     <div className="fixed top-0 left-0 h-dvh w-full flex justify-center items-center bg-black/50">
       <div className="bg-white rounded-md">{children}</div>
     </div>,
-    root,
+    root
   );
 };
 
